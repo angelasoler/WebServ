09e39628bb89a20618cbbb99f518a10e615a61d8
gsmereka <gsmereka200@gmail.com>
2024-07-22 15:02:50 -0300

    add-config.hpp

---
+++ config.hpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <vector>
+#include <map>
+
+struct RouteConfig
+{
+	std::string					path;
+	std::vector<std::string>	accepted_methods;
+	std::string					redirection;
+	std::string					root_directory;
+	bool 						directory_listing;
+	std::string 				default_file;
+	std::string 				cgi_extension;
+	std::string					upload_directory;
+};
+
+struct CGIConfig
+{
+	std::string path_info;
+	std::string script_path;
+};
+
+struct ServerConfig
+{
+	std::string					host;
+	int 						port;
+	std::vector<std::string>	server_names;
+	std::string					default_error_page;
+	size_t						client_body_limit;
+	CGIConfig					cgi;
+
+	std::map<std::string, RouteConfig>	routes;
+};
+
+class config
+{
+	public:
+		std::vector<ServerConfig> servers;
+
+		void addServer(const ServerConfig& server)
+		{
+			servers.push_back(server);
+		}
+};
