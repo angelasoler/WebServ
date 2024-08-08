665b41f5a465ccb2d09249f224a9e6489fb62d01
gsmereka <gsmereka200@gmail.com>
2024-08-08 13:59:38 -0300

    isolate-response

--- src/ParsePathInfo.cpp
+++ src/ParsePathInfo.cpp
@@ -6,13 +6,15 @@
 void	ParsePathInfo::parsePathInfo(RequestInfo &info)
 {
 	info.pathType = identifyPathType(info.requestedRoute, info.serverRef, info);
+
 	if (info.pathType == Redirection || info.pathType == File || info.pathType == CGI || info.pathType == Directory)
 		info.permissions = getPermissions(info.fullPath);
 }
 
-e_pathType identifyPathType(std::string& path, ServerConfig& serverConfig, RequestInfo &info)
+e_pathType identifyPathType(std::string& requestedRoute, ServerConfig& serverConfig, RequestInfo &info)
 {
-	if (serverConfig.cgi.path_info == path || serverConfig.cgi.script_path == path) {
+	if (serverConfig.cgi.path_info == requestedRoute || serverConfig.cgi.script_path == requestedRoute) {
+		info.fullPath = std::string(requestedRoute);
 		return CGI;
 	}
 
@@ -20,14 +22,18 @@ e_pathType identifyPathType(std::string& path, ServerConfig& serverConfig, Reque
 	std::map<std::string, RouteConfig>::const_iterator it;
 	for (it = serverConfig.routes.begin(); it != serverConfig.routes.end(); ++it)
 	{
-		const RouteConfig& route = it->second;
+		const RouteConfig& routeConfig = it->second;
 
 		// Se o caminho corresponde à rota configurada
-		if (path == route.route || path.find(route.route) == 0)
+		if ((requestedRoute == routeConfig.route))
 		{
-
+			info.fullPath = std::string(routeConfig.root_directory) + "/" + std::string(routeConfig.default_file);
+			return File;
+		}
+		else if (requestedRoute.find(routeConfig.route) == 0)
+		{
 			// Define o caminho completo pro caso de ser um diretoio ou arquivo
-			info.fullPath = std::string(route.root_directory) + std::string(path);
+			info.fullPath = std::string(routeConfig.root_directory) + std::string(requestedRoute);
 
 			// Verificar se é um diretório
 			if (isDirectory(info.fullPath))
@@ -38,13 +44,14 @@ e_pathType identifyPathType(std::string& path, ServerConfig& serverConfig, Reque
 				return File;
 
 			// Verificar se há uma redireção configurada
-			else if (!route.redirection.empty())
+			else if (!routeConfig.redirection.empty())
 				return Redirection;
 
 			// Verificar se o caminho está associado a um CGI
 			else if (endsWith(info.fullPath, DEFAULT_CGI_EXTENSION))
 				return CGI;
 		}
+		
 	}
 	// Se não encontrar nenhuma correspondência específica, tratar como uma URL padrão
 	return URL;
