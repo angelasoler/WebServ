8fffe8ee470e9932d97aa41658a8d793490a2542
gsmereka <gabrielsmereka200@gmail.com>
2024-08-16 16:11:34 -0300

    add-more-tests

--- test/webservtest.cpp
+++ test/webservtest.cpp
@@ -492,9 +492,33 @@ TEST(RequestInfoTest, HandlesGetRequest) {
 	EXPECT_EQ(requestInfo.body, "");
 }
 
+TEST(RequestInfoTest, HandlesPostRequestWithFormUrlEncoded) {
+	std::string postRequest = 
+		"POST " + std::string(DEFAULT_ROUTE_PATH) + " HTTP/1.1\r\n"
+		"Host: localhost\r\n"
+		"Content-Type: application/x-www-form-urlencoded\r\n"
+		"Content-Length: 29\r\n\r\n"
+		"username=testuser&password=1234";
+
+	RequestInfo requestInfo = parseHttpRequest(postRequest);
+
+	EXPECT_EQ(requestInfo.requestedRoute, DEFAULT_ROUTE_PATH "/login");
+	EXPECT_EQ(requestInfo.action, UPLOAD);
+	EXPECT_EQ(requestInfo.pathType, URL);
+	EXPECT_EQ(requestInfo.permissions.read, true);
+	EXPECT_EQ(requestInfo.permissions.write, true);
+	EXPECT_EQ(requestInfo.permissions.execute, false);
+	EXPECT_EQ(requestInfo.auto_index, false);
+	EXPECT_EQ(requestInfo.body, "username=testuser&password=1234");
+	ASSERT_FALSE(requestInfo.requestBody["username"].empty());
+	EXPECT_EQ(requestInfo.requestBody["username"][0], "testuser");
+	ASSERT_FALSE(requestInfo.requestBody["password"].empty());
+	EXPECT_EQ(requestInfo.requestBody["password"][0], "1234");
+}
+
 TEST(RequestInfoTest, HandlesPostRequest) {
 	std::string postRequest = 
-		"POST /submit HTTP/1.1\r\n"
+		"POST " + std::string(DEFAULT_ROUTE_PATH) + " HTTP/1.1\r\n"
 		"Host: localhost\r\n"
 		"Content-Type: application/x-www-form-urlencoded\r\n"
 		"Content-Length: 27\r\n\r\n"
@@ -503,7 +527,7 @@ TEST(RequestInfoTest, HandlesPostRequest) {
 	RequestInfo requestInfo = parseHttpRequest(postRequest);
 
 	// Verificando o preenchimento da struct
-	EXPECT_EQ(requestInfo.requestedRoute, "/submit");
+	EXPECT_EQ(requestInfo.requestedRoute, DEFAULT_ROUTE_PATH);
 	EXPECT_EQ(requestInfo.action, UPLOAD);
 	EXPECT_EQ(requestInfo.pathType, URL);
 	EXPECT_EQ(requestInfo.permissions.read, true);
@@ -511,21 +535,24 @@ TEST(RequestInfoTest, HandlesPostRequest) {
 	EXPECT_EQ(requestInfo.permissions.execute, false);
 	EXPECT_EQ(requestInfo.auto_index, false);
 	EXPECT_EQ(requestInfo.body, "name=John&age=30&city=NYC");
-	// EXPECT_EQ(requestInfo.requestBody["name"][0], "John");
-	// EXPECT_EQ(requestInfo.requestBody["age"][0], "30");
-	// EXPECT_EQ(requestInfo.requestBody["city"][0], "NYC");
+	ASSERT_FALSE(requestInfo.requestBody["name"].empty());
+	EXPECT_EQ(requestInfo.requestBody["name"][0], "John");
+	ASSERT_FALSE(requestInfo.requestBody["age"].empty());
+	EXPECT_EQ(requestInfo.requestBody["age"][0], "30");
+	ASSERT_FALSE(requestInfo.requestBody["city"].empty());
+	EXPECT_EQ(requestInfo.requestBody["city"][0], "NYC");
 }
 
 TEST(RequestInfoTest, HandlesDeleteRequest) {
 	std::string deleteRequest = 
-		"DELETE /remove HTTP/1.1\r\n"
+		"DELETE " + std::string(DEFAULT_FILE) + " HTTP/1.1\r\n"
 		"Host: localhost\r\n"
 		"Connection: close\r\n\r\n";
 
 	RequestInfo requestInfo = parseHttpRequest(deleteRequest);
 
 	// Verificando o preenchimento da struct
-	EXPECT_EQ(requestInfo.requestedRoute, "/remove");
+	EXPECT_EQ(requestInfo.requestedRoute, DEFAULT_ROUTE_PATH);
 	EXPECT_EQ(requestInfo.action, DELETE);
 	EXPECT_EQ(requestInfo.pathType, File);
 	EXPECT_EQ(requestInfo.permissions.read, false);
@@ -534,6 +561,56 @@ TEST(RequestInfoTest, HandlesDeleteRequest) {
 	EXPECT_EQ(requestInfo.auto_index, false);
 	EXPECT_EQ(requestInfo.body, "");
 }
+
+TEST(RequestInfoTest, HandlesPostRequestWithHtmlBody) {
+	std::string postRequest = 
+		"POST " + std::string(DEFAULT_ROUTE_PATH) + " HTML HTTP/1.1\r\n"
+		"Host: localhost\r\n"
+		"Content-Type: text/html\r\n"
+		"Content-Length: 51\r\n\r\n"
+		"<html><body><p>This is a test</p></body></html>";
+
+	RequestInfo requestInfo = parseHttpRequest(postRequest);
+
+	EXPECT_EQ(requestInfo.requestedRoute, DEFAULT_ROUTE_PATH);
+	EXPECT_EQ(requestInfo.action, UPLOAD);
+	EXPECT_EQ(requestInfo.pathType, URL);
+	EXPECT_EQ(requestInfo.permissions.read, true);
+	EXPECT_EQ(requestInfo.permissions.write, true);
+	EXPECT_EQ(requestInfo.permissions.execute, false);
+	EXPECT_EQ(requestInfo.auto_index, false);
+	EXPECT_EQ(requestInfo.body, "<html><body><p>This is a test</p></body></html>");
+}
+
+TEST(RequestInfoTest, HandlesPostRequestWithMultipartFormData) {
+	std::string boundary = "--boundary";
+	std::string postRequest = 
+		"POST " + std::string(DEFAULT_ROUTE_PATH) + " HTTP/1.1\r\n"
+		"Host: localhost\r\n"
+		"Content-Type: multipart/form-data; boundary=boundary\r\n"
+		"Content-Length: 144\r\n\r\n" +
+		boundary + "\r\n" +
+		"Content-Disposition: form-data; name=\"field1\"\r\n\r\n" +
+		"value1\r\n" + 
+		boundary + "\r\n" +
+		"Content-Disposition: form-data; name=\"file\"; filename=\"test.txt\"\r\n" +
+		"Content-Type: text/plain\r\n\r\n" +
+		"file content here\r\n" + 
+		boundary + "--";
+
+	RequestInfo requestInfo = parseHttpRequest(postRequest);
+
+	EXPECT_EQ(requestInfo.requestedRoute, DEFAULT_ROUTE_PATH);
+	EXPECT_EQ(requestInfo.action, UPLOAD);
+	EXPECT_EQ(requestInfo.pathType, URL);
+	EXPECT_EQ(requestInfo.permissions.read, true);
+	EXPECT_EQ(requestInfo.permissions.write, true);
+	EXPECT_EQ(requestInfo.permissions.execute, false);
+	EXPECT_EQ(requestInfo.auto_index, false);
+	EXPECT_TRUE(requestInfo.body.find("value1") != std::string::npos);
+	EXPECT_TRUE(requestInfo.body.find("file content here") != std::string::npos);
+}
+
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
