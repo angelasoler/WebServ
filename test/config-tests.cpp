1d3b4254bac4b5b42502f9ed18dca64151613e5a
gsmereka <gabrielsmereka200@gmail.com>
2024-08-22 21:04:28 -0300

    att-Config.hpp

--- test/config-tests.cpp
+++ test/config-tests.cpp
@@ -49,6 +49,7 @@ TEST(ConfigTest, NoServers) {
 	// Verificação da rota do primeiro servidor
 	ASSERT_EQ(server1.routes.empty(), false);
 
+	
 	return ;
 	const RouteConfig& route1 = server1.routes.at(DEFAULT_ROUTE_PATH);
 	EXPECT_EQ(route1.route, DEFAULT_ROUTE_PATH);
@@ -68,7 +69,7 @@ TEST(ConfigTest, OneServer) {
 
 	config->loadConfig("./test/conf_test_files/OneServer.conf");
 
-	// printConfig(*config);
+	// printConfig();
 	// return ;
 	ASSERT_EQ(config->servers.size(), 1);
 
@@ -302,32 +303,23 @@ TEST(ConfigTest, InvalidServerId1)
 	config->loadConfig("./test/conf_test_files/InvalidServerId1.conf");
 	ASSERT_EQ(config->servers.size(), 1);
 
-	// Verificação do primeiro servidor
 	const ServerConfig& server1 = config->servers[0];
-	EXPECT_EQ(server1.host, "localhost");
-	EXPECT_EQ(server1.port, 8080);
-	EXPECT_EQ(server1.server_names[0], "example_x.com");
-	EXPECT_EQ(server1.default_error_page, "/errors/404.html");
-	EXPECT_EQ(server1.client_body_limit, 1048576);
+	EXPECT_EQ(server1.host, DEFAULT_HOST);
+	EXPECT_EQ(server1.port, DEFAULT_PORT);
+	EXPECT_EQ(server1.server_names[0], DEFAULT_SERVER_NAME);
+	EXPECT_EQ(server1.default_error_page, DEFAULT_ERROR_PAGE);
+	EXPECT_EQ(server1.client_body_limit, DEFAULT_CLIENT_BODY_LIMIT);
 
-	// Verificação da rota do primeiro servidor
 	ASSERT_EQ(server1.routes.size(), 1);
-	const RouteConfig& route1 = server1.routes.at("/");
-	EXPECT_EQ(route1.route, "/");
-	EXPECT_EQ(route1.redirection, "/route_x_redirection");
-	EXPECT_EQ(route1.root_directory, "/var/www/route_x");
-	EXPECT_EQ(route1.accepted_methods.size(), 3);
+	const RouteConfig& route1 = server1.routes.at(DEFAULT_ROUTE_PATH);
+	EXPECT_EQ(route1.route, DEFAULT_ROUTE_PATH);
+	EXPECT_EQ(route1.redirection, DEFAULT_REDIRECTION);
+	EXPECT_EQ(route1.root_directory, DEFAULT_ROOT_DIRECTORY);
+	EXPECT_EQ(route1.accepted_methods.size(), 2);
 	EXPECT_EQ(route1.accepted_methods[0], "GET");
-	EXPECT_EQ(route1.accepted_methods[1], "POST");
-	EXPECT_EQ(route1.accepted_methods[2], "DELETE");
-	EXPECT_EQ(route1.directory_listing, true);
-	EXPECT_EQ(route1.default_file, "index.html");
-	EXPECT_EQ(route1.cgi_extension, ".php");
-	EXPECT_EQ(route1.upload_directory, "/var/www/uploads");
-
-	// Verificação do CGI do primeiro servidor
-	EXPECT_EQ(server1.cgi.path_info, "/cgi/script");
-	EXPECT_EQ(server1.cgi.script_path, "/cgi/script.cgi");
+	EXPECT_EQ(route1.accepted_methods[1], "DELETE");
+	EXPECT_EQ(route1.directory_listing, DEFAULT_DIRECTORY_LISTING);
+	EXPECT_EQ(route1.default_file, DEFAULT_FILE);
 }
 
 TEST(ConfigTest, InvalidServerId1ValidServerId2) {
@@ -338,28 +330,22 @@ TEST(ConfigTest, InvalidServerId1ValidServerId2) {
 
 	// Verificação do primeiro servidor
 	const ServerConfig& server1 = config->servers[0];
-	EXPECT_EQ(server1.host, "localhost");
-	EXPECT_EQ(server1.port, 8080);
-	EXPECT_EQ(server1.server_names[0], "example_x.com");
-	EXPECT_EQ(server1.default_error_page, "/errors/404.html");
-	EXPECT_EQ(server1.client_body_limit, 1048576);
+	EXPECT_EQ(server1.host, DEFAULT_HOST);
+	EXPECT_EQ(server1.port, DEFAULT_PORT);
+	EXPECT_EQ(server1.server_names[0], DEFAULT_SERVER_NAME);
+	EXPECT_EQ(server1.default_error_page, DEFAULT_ERROR_PAGE);
+	EXPECT_EQ(server1.client_body_limit, DEFAULT_CLIENT_BODY_LIMIT);
 
 	ASSERT_EQ(server1.routes.size(), 1);
-	const RouteConfig& route1 = server1.routes.at("/route_x");
-	EXPECT_EQ(route1.route, "/route_x");
-	EXPECT_EQ(route1.redirection, "/route_x_redirection");
-	EXPECT_EQ(route1.root_directory, "/var/www/route_x");
-	EXPECT_EQ(route1.accepted_methods.size(), 3);
+	const RouteConfig& route1 = server1.routes.at(DEFAULT_ROUTE_PATH);
+	EXPECT_EQ(route1.route, DEFAULT_ROUTE_PATH);
+	EXPECT_EQ(route1.redirection, DEFAULT_REDIRECTION);
+	EXPECT_EQ(route1.root_directory, DEFAULT_ROOT_DIRECTORY);
+	EXPECT_EQ(route1.accepted_methods.size(), 2);
 	EXPECT_EQ(route1.accepted_methods[0], "GET");
-	EXPECT_EQ(route1.accepted_methods[1], "POST");
-	EXPECT_EQ(route1.accepted_methods[2], "DELETE");
-	EXPECT_EQ(route1.directory_listing, true);
-	EXPECT_EQ(route1.default_file, "index.html");
-	EXPECT_EQ(route1.cgi_extension, ".php");
-	EXPECT_EQ(route1.upload_directory, "/var/www/uploads");
-
-	EXPECT_EQ(server1.cgi.path_info, "/cgi/script");
-	EXPECT_EQ(server1.cgi.script_path, "/cgi/script.cgi");
+	EXPECT_EQ(route1.accepted_methods[1], "DELETE");
+	EXPECT_EQ(route1.directory_listing, DEFAULT_DIRECTORY_LISTING);
+	EXPECT_EQ(route1.default_file, DEFAULT_FILE);
 
 	// Verificação do terceiro servidor
 	const ServerConfig& server3 = config->servers[1];
@@ -435,6 +421,7 @@ TEST(ConfigTest, TwoServersTwoRoutes)
 	Config  *config = Config::getInstance();
 
 	config->loadConfig("./test/conf_test_files/TwoServersTwoRoutes.conf");
+	printConfig();
 	ASSERT_EQ(config->servers.size(), 2);
 
 	const ServerConfig& server = config->servers[0];
